dc0cf4a9fb02b07376cbda08205b2a60bf327330
brujoand <anders@brujordet.no>
2016-03-09 12:08:24 +0100

    Adding installscript

---
+++ install.bash
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+base_path=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+if [[ ! -f "$HOME"/.sbp ]]; then
+  echo "Adding default config to $HOME/.sbp"
+  cp "$base_path"/settings.default "$HOME/.sbp"
+else
+  echo "Found config at $HOME/.sbp"
+fi
+
+if [[ -z "$sbp_path" || "$base_path" != "$sbp_path" ]]; then 
+  echo "Please add the following to your .bashrc or equivalent"
+  echo "sbp_path=${base_path}"
+  echo "source ${base_path}/sbp.bash"
+else
+  echo "Everything seems to be in order"
+fi
