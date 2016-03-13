3ccfdfa51fd292eb3dd9c926e86a0a1224d134a1
brujoand <anders@brujordet.no>
2016-03-13 15:49:12 +0100

    Fix the default timestamp value

--- segments/timestamp.bash
+++ segments/timestamp.bash
@@ -1,6 +1,7 @@
 ### Defaults
 _sbp_timestamp_color_bg=${_sbp_timestamp_color_bg:-$_sbp_color_dgrey}
 _sbp_timestamp_color_fg=${_sbp_timestamp_color_fg:-$_sbp_color_lgrey}
+_sbp_timestamp_format=${_sbp_timestamp_format:-"%H:%M:%S"}
 
 function _sbp_generate_timestamp_segment {
   local timestamp_color timestamp_value
