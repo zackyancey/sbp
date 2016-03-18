9b8b482d81ca99fa5872784de55b650c697bb986
brujoand <anders@brujordet.no>
2016-03-18 09:18:25 +0100

    Remove extra pyenv nested check

--- segments/python_virtual_env.bash
+++ segments/python_virtual_env.bash
@@ -2,24 +2,12 @@
 _sbp_python_virtual_env_bg=${_sbp_python_virtual_env_bg:-$_sbp_color_dgreen}
 _sbp_python_virtual_env_fg=${_sbp_python_virtual_env_fg:-$_sbp_color_white}
 
-function _find_pyenv_version() {
-  local path=${PWD%/*}
-  while [[ $path ]]; do
-    if [[ -f "${path}/.python-version" ]]; then
-      cat "${path}/.python-version"
-      break 
-    else
-      path=${path%/*}
-    fi
-  done
-}
-
 function _sbp_generate_python_virtual_env_segment() {
   local pyenv_value segment_value segment_value
   virtualenv_value="${VIRTUAL_ENV##*/}"
   pyenv_value=$(pyenv local 2>/dev/null)
   segment_value="${virtualenv_value}${pyenv_value}"
-  
+
   if [[ -n "$segment_value" ]]; then
     _sbp_segment_new_color_bg="$_sbp_python_virtual_env_bg"
     _sbp_segment_new_color_fg="$_sbp_python_virtual_env_fg"
