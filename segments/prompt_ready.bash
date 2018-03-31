991175f0b8e7e3b5c5fe75a75eb9028ca0aa22e8
brujoand <anders@brujordet.no>
2018-03-31 15:12:27 +0200

    Let the bg color reset properly after on last segment

--- segments/prompt_ready.bash
+++ segments/prompt_ready.bash
@@ -2,6 +2,6 @@
 
 segment_direction=$3
 
-pretty_print_segment "$settings_prompt_ready_color" '0' " ${settings_char_ready} " "$segment_direction"
+pretty_print_segment "$settings_prompt_ready_color" -1 " ${settings_char_ready} " "$segment_direction"
 
 
